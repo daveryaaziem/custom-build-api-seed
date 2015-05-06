4b49863939d935b56f9e4bd9d2fa38a6d07575e2
Dave Smith <daveryaaziem@hotmail.com>
2015-05-05 22:30:42 -0700

    Change Vagrantfile and install_script to work woith cartoons db

--- install_script.sh
+++ install_script.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+	#!/bin/bash
 
 # install web server dependencies
 sudo apt-get update
 sudo apt-get -y install python python-virtualenv nginx supervisor
 
 # install application (source location in $1)
-mkdir /home/vagrant/students
+mkdir /home/vagrant/cartoons
 cp $1/requirements.txt /home/vagrant
-cp -R $1/students/* /home/vagrant/students/
+cp -R $1/cartoons/* /home/vagrant/cartoons/
 
 # create a virtualenv and install dependencies
-virtualenv /home/vagrant/students/venv
-source /home/vagrant/students/venv/bin/activate
-sudo /home/vagrant/students/venv/bin/pip install -r /home/vagrant/requirements.txt
+virtualenv /home/vagrant/cartoons/venv
+source /home/vagrant/cartoons/venv/bin/activate
+sudo /home/vagrant/cartoons/venv/bin/pip install -r /home/vagrant/requirements.txt
 
 # configure supervisor
-sudo cp /vagrant/student.conf /etc/supervisor/conf.d/
-sudo mkdir /var/log/student
+sudo cp /vagrant/cartoon.conf /etc/supervisor/conf.d/
+sudo mkdir /var/log/cartoon
 sudo supervisorctl reread
 sudo supervisorctl update
 
 # configure nginx
-sudo cp /vagrant/student.nginx /etc/nginx/sites-available/student
+sudo cp /vagrant/cartoons.nginx /etc/nginx/sites-available/cartoon
 sudo rm -f /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/student /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/cartoon/etc/nginx/sites-enabled/
 sudo service nginx restart
 
 echo Application deployed to http://localhost:5000/
